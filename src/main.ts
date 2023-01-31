4c122e8c54df86938449cf07f0d38ff03bcee671
lucas.rodrigues <lucasrodrigues22020@gmail.com>
2023-01-31 12:27:36 -0300

    Alter ports

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const porta = 3000
+  const porta = process.env.PORT || 3000
   await app.listen(porta, () => console.log(`Servidor rodando na porta ${porta}`));
 }
 bootstrap();
